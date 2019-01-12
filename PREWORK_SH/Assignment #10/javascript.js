6b2c4fe5149097c6fd4e7f90afd011e6b0d2dc4f
Scott H <zenwattage@gmail.com>
2019-01-12 12:44:06 -0800

    push from desktop

---
+++ PREWORK_SH/Assignment #10/javascript.js
@@ -0,0 +1,18 @@
+function growFunction() {
+    // find way to target both in 1 line
+    document.getElementById("box").style.height = "250px";
+    document.getElementById("box").style.width = "250px";
+
+}
+
+function blueFunction() {
+    document.getElementById("box").style.backgroundColor = "blue";
+}
+
+function fadeFunction() {
+    document.getElementById("box").style.opacity = "0.5";
+}
+
+function resetFunction() {
+    location.reload();
+}
